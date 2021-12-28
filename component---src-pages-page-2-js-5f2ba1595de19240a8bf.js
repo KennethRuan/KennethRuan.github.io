d252d599b3c74d91ec878fd42b72782c747944f4
KennethRuan <kennethjruan@gmail.com>
2021-12-28 01:27:44 -0500

    Updates

---
+++ component---src-pages-page-2-js-5f2ba1595de19240a8bf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{9863:function(e,t,a){a.r(t);var l=a(7294);a(5444),a(7198),a(3751);t.default=function(){return l.createElement("p",null,"Hello World")}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-5f2ba1595de19240a8bf.js.map
