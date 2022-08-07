f2bdb6701df42b30f7dce2712fa853bfc9b38fe8
KennethRuan <kennethjruan@gmail.com>
2022-08-06 23:25:37 -0400

    fixed issue with wrapping divs in anchor tag

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -1,6 +1,6 @@
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react'
-import {cardsGrid, cardContainer, cardTop, cardContent, projectTitle, projectDescription, projectTags, folder} from '../../styles/projects.module.css'
+import {cardsGrid, cardContainer, cardTop, cardContent, projectTitle, projectDescription, projectTags, folder, externalRef} from '../../styles/projects.module.css'
 import '../../styles/global.css'
 
 
@@ -27,32 +27,30 @@ const Projects = () => {
       
     const projects = data.allMarkdownRemark.nodes
 
-
     return ( 
         <div className={"section"} id="projects">
             <h1>Projects</h1>
             <div className={cardsGrid}>
                 {projects.map(project=>(
-                <a href = {project.frontmatter.github} target="_blank" rel="noreferrer">
-                    <div className={cardContainer}>
-                        <div className={cardTop}>
-                            <img className={folder} src="./folder.svg"/>
-                        </div>
-                        <div className={cardContent}>
-                            <div className={projectTitle}>
-                                <a href={project.frontmatter.github} target="_blank" rel="noreferrer">
-                                    {project.frontmatter.title}
-                                </a>
-                            </div>
-                            <div className={projectDescription} dangerouslySetInnerHTML={{__html:project.html}} />
-                            <ul className={projectTags}>
-                                {project.frontmatter.tech.map((tech) => (
-                                    <li> {tech} </li>
-                                ))}
-                            </ul>
-                        </div>
+                <div className={cardContainer}>
+                    {/* <a className={externalRef} href={project.frontmatter.github} target="_blank" rel="noreferrer"></a>  */}
+                    <div className={cardTop}>
+                        <img className={folder} src="./folder.svg"/>
                     </div>
-                </a>
+                    <div className={cardContent}>
+                        <div className={projectTitle}>
+                            <a href={project.frontmatter.github} target="_blank" rel="noreferrer">
+                                {project.frontmatter.title}
+                            </a>
+                        </div>
+                        <div className={projectDescription} dangerouslySetInnerHTML={{__html:project.html}} />
+                        <ul className={projectTags}>
+                            {project.frontmatter.tech.map((tech) => (
+                                <li> {tech} </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
                 ))}
             </div>
         </div>
